02137a423ad8bde743286bf5856e1a358e9adb55
syed mukheed <syedmukheed777@gmail.comm>
2022-11-02 12:00:50 +0400

    change some codes

--- security-group.tf
+++ security-group.tf
@@ -5,27 +5,27 @@ resource "aws_security_group" "alb-security-group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "HTTP Access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "HTTPS Access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-   }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "ALB Security Group"
@@ -39,19 +39,19 @@ resource "aws_security_group" "ssh-security-group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "SSH Access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["${var.ssh-location}"]
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ssh-location}"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-   }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "SSH Security Group"
@@ -65,35 +65,35 @@ resource "aws_security_group" "webserver-security-group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "HTTP Access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    security_groups  = ["${aws_security_group.alb-security-group.id}"]
+    description     = "HTTP Access"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.alb-security-group.id}"]
   }
 
   ingress {
-    description      = "HTTPS Access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    security_groups  = ["${aws_security_group.alb-security-group.id}"]
+    description     = "HTTPS Access"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.alb-security-group.id}"]
   }
 
   ingress {
-    description      = "SSH Access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    security_groups  = ["${aws_security_group.ssh-security-group.id}"]
+    description     = "SSH Access"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.ssh-security-group.id}"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-   }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "Web Server Security Group"
@@ -107,19 +107,19 @@ resource "aws_security_group" "database-security-group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "MySQL/Aurora Access"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    security_groups  = ["${aws_security_group.webserver-security-group.id}"]
+    description     = "MySQL/Aurora Access"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.webserver-security-group.id}"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-   }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "DataBase Security Group"
